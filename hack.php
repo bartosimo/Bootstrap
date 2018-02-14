5c883fdbcd5ae3d73d4ba9f5ca13aa2e406989bc
bartosimo <bartosimo00@gmail.com>
2018-02-14 17:17:05 +0100

    Con Mr.K Organization

--- hack.php
+++ hack.php
@@ -1,13 +1,14 @@
 <html>
 <br/>
 <br/>
-<body> <?php if (isset($_REQUEST['submitted']) && $_REQUEST['submitted'] == '1') {
+<body> <?php if (isset($_REQUEST['submitted']) && $_REQUEST['submitted'] == '100') {
     echo "Form submitted!";
 } ?>
-<form action="<?php echo $_SERVER['PHP_SELF']; ?>">
-    <input type="hidden" name="submitted" value="1"/>
+    <form action="<?php echo $_SERVER['PHP_SELF']; ?>">
+    <input type="hidden" name="reset" value="1"/>
     <input type="submit" value="Submit!"/>
 </form>
 
 
 </body>
+</html>
